16bfec76340599a5040e6a9be1472591b44f6edd
natostanco <bassobassista@gmail.com>
2016-04-26 13:05:18 +0200

    .

--- .images/c3cb62377b4d92d57a8edbdec4072295971bafbe61b000210c07895c8fad220c/artifacts/files/entry.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mkdir -p /var/log/redis /etc/redis/${HOSTNAME} && cp /etc/redis/*.conf /etc/redis/${HOSTNAME} && chown -R redis:999 /var/lib/redis /tmp /var/log/redis && redis-server /etc/redis/${HOSTNAME}/redis.conf
