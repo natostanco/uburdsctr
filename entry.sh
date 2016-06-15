74e1ea89c0590ec2df922e6b43083ac407bbee58
Francesco Giannelli <bassobassista@gmail.com>
2016-06-15 12:06:51 +0000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir -p /var/log/redis /etc/redis/${HOSTNAME}
-cp /etc/redis/*.conf /etc/redis/${HOSTNAME}
+mkdir -p /var/log/redis
 chown -R redis:999 /var/lib/redis /tmp /var/log/redis 
-rdsconf=`find /etc/redis/${HOSTNAME} -name "redis.conf"`
-redis-server $rdsconf
+redis-server /etc/redis/redis.conf
